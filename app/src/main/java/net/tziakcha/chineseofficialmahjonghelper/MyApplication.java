3343691d2ea5724ddc1c84e12d402a3eaa6296ed
Jeff Wang <summer_insects@163.com>
2026-05-07 09:04:31 +0800

    no message

---
+++ app/src/main/java/net/tziakcha/chineseofficialmahjonghelper/MyApplication.java
@@ -0,0 +1,14 @@
+package net.tziakcha.chineseofficialmahjonghelper;
+
+import android.app.Application;
+
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+    }
+}
